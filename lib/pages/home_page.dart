0df00d53f899727ceda57d8fa3914df66dff7e58
aryanpamwani123 <aryanpamwani123@gmail.com>
2023-12-26 14:47:39 +0000

    Day 1

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hello"),
+      ),
+      drawer: const Drawer(),
+      body: const Center(
+        child: Text("Welcome tò 30 days of Flutter "),
+      ),
+    );
+  }
+}
